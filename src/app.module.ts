cdac2b0800c6f396de57d896a57fea7c7ba21f84
Strangexz <ja_figueroa@outlook.es>
2024-09-06 18:52:36 -0600

    feat: conexion a mongo establecida

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+    MongooseModule.forRoot(
+      'mongodb+srv://jafigueroa:H1McmZ9tONwJO2B8@cluster0.8dojn.mongodb.net/',
+    ),
     PokemonModule,
   ],
   controllers: [],
