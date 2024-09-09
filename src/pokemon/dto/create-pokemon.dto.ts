282bd76ef338149dd6befc447ff6836b9b41a929
Strangexz <ja_figueroa@outlook.es>
2024-09-09 11:00:45 -0600

    feat: conexión e inserción a BD

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  // isInt, IsPositive
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
