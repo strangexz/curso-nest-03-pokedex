8fa94f2d71faa0f4cf999b16dba3b5640738084f
Strangexz <ja_figueroa@outlook.es>
2024-09-09 16:54:07 -0600

    test: modificado test de servicio

--- src/pokemon/pokemon.service.spec.ts
+++ src/pokemon/pokemon.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PokemonService } from './pokemon.service';
+import { Pokemon } from './entities/pokemon.entity';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 describe('PokemonService', () => {
   let service: PokemonService;
+  let model: Model<Pokemon>;
+
+  // const pokemons: Pokemon[] = [
+  //   {
+  //     no: 1,
+  //     name: 'bulbasur',
+  //   },
+  // ];
+
+  const mockPokemonModel = () => ({
+    create: jest.fn(),
+    find: () => ({ lean: jest.fn().mockResolvedValueOnce([]) }),
+    findOne: jest.fn(),
+    findById: jest.fn(),
+    deleteOne: jest.fn(),
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PokemonService],
+      providers: [
+        PokemonService,
+        { provide: getModelToken('Pokemon'), useFactory: mockPokemonModel },
+      ],
     }).compile();
 
     service = module.get<PokemonService>(PokemonService);
+    model = module.get(getModelToken('Pokemon'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // describe('findAll', () => {
+  //   it('should get all gets in service', async () => {
+  //     const allPokemon = await service.findAll();
+  //     expect(allPokemon.length).toStrictEqual(1);
+  //   });
+  // });
 });
